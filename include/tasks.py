7fa64b2bb4ff30df621ea68521d042eb7ec0e0c2
mahufa <huflejtm@gmail.com>
2025-08-29 12:50:02 +0200

    Add make_check_api_task function

--- include/tasks.py
+++ include/tasks.py
@@ -1,9 +1,27 @@
+from typing import Callable
+
+from airflow.decorators import task
 from airflow.exceptions import AirflowException
 from airflow.sensors.base import PokeReturnValue
 from airflow.utils.log.logging_mixin import LoggingMixin
 
 from include.helpers.api_client import get_api_hook
 
+
+def make_check_api_task(
+    poke_interval=30,
+    timeout=300,
+    mode="reschedule"
+) -> Callable:
+    @task.sensor(
+        poke_interval=poke_interval,
+        timeout=timeout,
+        mode=mode
+    )
+    def _check_api_task() -> PokeReturnValue:
+        return _check_api()
+
+    return _check_api_task
 
 def _check_api() -> PokeReturnValue:
     from requests import RequestException
