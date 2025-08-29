d6f7fe8d0469900f113c5e009a0d85c2c82004dd
mahufa <huflejtm@gmail.com>
2025-08-29 13:22:24 +0200

    Move checking api logic to api_client helper

--- include/helpers/api_client.py
+++ include/helpers/api_client.py
@@ -1,16 +1,36 @@
+from logging import getLogger
+
+from airflow.exceptions import AirflowException
 from airflow.providers.http.hooks.http import HttpHook
 
 
 def get_api_data(
     endpoint: str,
     params: dict = None,
-) -> str:
+) -> dict:
     api = get_api_hook()
     response = api.run(
         endpoint=endpoint,
-        data=params,
+        data=params, #HttpHook expects GET params passed via `data=...`
     )
     return response.json()
+
+
+def is_api_available(api: HttpHook) -> bool:
+    from requests import RequestException
+
+    try:
+        api.run(
+            endpoint='/Line/Meta/Modes',
+            extra_options={'timeout': (3.0, 5.0)},
+        )
+    except (RequestException, AirflowException):
+        logger = getLogger(__name__)
+        logger.exception("Failed to connect to API")
+        return False
+    else:
+        return True
+
 
 def get_api_hook() -> HttpHook:
     return HttpHook(
--- include/tasks.py
+++ include/tasks.py
@@ -1,11 +1,9 @@
 from typing import Callable
 
 from airflow.decorators import task
-from airflow.exceptions import AirflowException
 from airflow.sensors.base import PokeReturnValue
-from airflow.utils.log.logging_mixin import LoggingMixin
 
-from include.helpers.api_client import get_api_hook
+from include.helpers.api_client import get_api_hook, is_api_available
 
 
 def make_check_api_task(
@@ -24,17 +22,7 @@ def make_check_api_task(
     return _check_api_task
 
 def _check_api() -> PokeReturnValue:
-    from requests import RequestException
     api = get_api_hook()
-    try:
-        api.run(
-            endpoint='/Line/Meta/Modes',
-            extra_options = {'timeout': (3.0, 5.0)},
-        )
-    except (RequestException, AirflowException):
-        LoggingMixin().log.exception('Failed to connect the API')
-        condition = False
-    else:
-        condition = True
+    condition = is_api_available(api)
 
     return PokeReturnValue(is_done=condition)
