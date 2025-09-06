45aec31643e4202d8a3dfc06f0ac4b6610b3bde1
mahufa <huflejtm@gmail.com>
2025-09-06 17:18:02 +0200

    Add dataset_utils module

---
+++ include/helpers/dataset_utils.py
@@ -0,0 +1,25 @@
+from airflow.models.dataset import DatasetEvent
+
+from include.datasets import PATH_KEY
+
+
+def get_events_paths(triggering_dataset_events: dict) -> dict[str, list[str]]:
+    paths_grouped_by_datasets = {
+        dataset_uri: _get_paths_from_events_list(event_list)
+        for dataset_uri, event_list in triggering_dataset_events.items()
+    }
+    return paths_grouped_by_datasets
+
+
+def _get_paths_from_events_list(event_list: list[DatasetEvent]) -> list[str]:
+    paths = [_get_path_from_event(event) for event in event_list]
+    return paths
+
+
+def _get_path_from_event(event: DatasetEvent) -> str:
+    path = event.extra.get(PATH_KEY)
+    if path is None:
+        raise KeyError(
+            f'Path not found in metadata for dataset {event.uri}'
+        )
+    return path
