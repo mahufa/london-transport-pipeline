d6c43b24c6a4191c8150506ec6ff879986bcc944
mahufa <huflejtm@gmail.com>
2025-09-10 16:38:23 +0200

    Add factory-functions for fetching data from storage

--- include/tasks/transform_tasks.py
+++ include/tasks/transform_tasks.py
@@ -1,8 +1,38 @@
 from typing import Callable
 
+from airflow.datasets import Dataset
 from airflow.decorators import task
 
 
+
+def make_fetch_stored_raw_task(
+    dataset: Dataset
+) -> Callable:
+    @task(
+        task_id=f'fetch_stored__{dataset.uri.split(sep="/")[0]}',
+    )
+    def _fetch_stored_raw(path: str) -> str:
+        from include.helpers.storage import read_str_from_s3
+
+        return read_str_from_s3(path)
+
+    return _fetch_stored_raw
+
+
+def make_extract_dataset_paths_task(
+    dataset: Dataset,
+    upstream_task_id: str = '_get_paths_to_raw'
+) -> Callable:
+
+    @task(
+        task_id=f'extract_paths_to__{dataset.uri.split(sep="/")[0]}',
+    )
+    def _extract_dataset_paths(ti=None) -> list[str]:
+        all_paths = ti.xcom_pull(task_ids=upstream_task_id)
+        specific_dataset_paths = all_paths.get(dataset.uri, [])
+        return specific_dataset_paths
+
+    return _extract_dataset_paths
 
 
 def make_get_paths_to_raw_task() -> Callable:
