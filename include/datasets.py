7c39b70548691a099f2a0b1bfc953f16de1fc66e
mahufa <huflejtm@gmail.com>
2025-09-12 14:27:34 +0200

    Refactor transform dag, pull 'extract' task out of 'process' group

--- include/datasets.py
+++ include/datasets.py
@@ -4,5 +4,11 @@ DATASET_BIKES = Dataset('bike_points/raw/')
 DATASET_CHARGERS = Dataset('chargers/raw/')
 DATASET_ROADS = Dataset('roads/raw/')
 
+ALL_DATASETS = [
+    DATASET_BIKES,
+    DATASET_CHARGERS,
+    DATASET_ROADS,
+]
+
 
 PATH_KEY = 'file_path'
