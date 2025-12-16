61f7d1501cf402927a57052f528f34a86ddde3fe
mahufa <huflejtm@gmail.com>
2025-12-16 15:51:02 +0100

    feat: add Postgres helpers and load DAG

---
+++ dags/load.py
@@ -0,0 +1,32 @@
+from airflow.decorators import dag
+from pendulum import duration
+
+from include.callbacks import notify_teams
+from include.dag_config import START_DATE
+from include.datasets import DATASETS
+from include.tasks.common_tasks import make_get_paths_to_triggering_data_task, make_extract_dataset_paths_task
+from include.tasks.load_tasks import build_staging_dataset_flow
+
+
+@dag(
+    dag_id='loader',
+    start_date=START_DATE,
+    schedule=(
+            DATASETS.get('bike_points').staging
+            | DATASETS.get('chargers').staging
+            | DATASETS.get('roads').staging
+    ),
+    catchup=False,
+    description=f'This DAG loads and models tfl data',
+    tags=['tfl', 'load'],
+    default_args={
+        'retries': 2,
+        'on_failure_callback': notify_teams,
+    },
+    dagrun_timeout=duration(minutes=10),
+    max_consecutive_failed_dag_runs=2,
+)
+def load():
+    all_staging_paths = make_get_paths_to_triggering_data_task()()
+
+load()
---
+++ include/helpers/postgres.py
@@ -0,0 +1,50 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from botocore.response import StreamingBody
+from psycopg2._psycopg import connection
+
+
+def copy_s3_obj_to_postgres(
+    s3_obj,
+    table_name: str,
+    batch_id: str,
+):
+    stream = s3_obj.get()['Body']
+    conn = _get_pg_conn()
+
+    try:
+        _remove_batch_from_staging(table_name, batch_id, conn)
+        _load_stream_to_pg(table_name, stream, conn)
+    finally:
+        conn.close()
+
+
+def _remove_batch_from_staging(
+    table_name: str,
+    batch_id: str,
+    conn: connection,
+):
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(f"DELETE FROM {table_name} WHERE batch_id='{batch_id}'")
+
+
+def _load_stream_to_pg(
+    table_name: str,
+    stream: StreamingBody,
+    conn: connection,
+):
+    with  conn:
+        with conn.cursor() as cur:
+            cur.copy_expert(f'COPY {table_name} FROM STDIN CSV HEADER', stream)
+
+
+
+def _get_pg_conn() -> connection:
+    hook = _get_pg_hook()
+    return hook.get_conn()
+
+
+def _get_pg_hook() -> PostgresHook:
+    return PostgresHook(
+        postgres_conn_id='postgres_dw',
+    )
